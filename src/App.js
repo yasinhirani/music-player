05d4a2e404d212acc251d708e825ea48fb2e924a
Yasin Hirani <yasin.hirani@1rivet.local>
2022-01-11 18:30:55 +0530

    Add: progress bar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {app, storage} from './Firebase';
 import { ref, getDownloadURL, listAll } from 'firebase/storage';
 import './App.css';
@@ -9,15 +9,19 @@ function App() {
   const [songUrl, setSongUrl] = useState("");
   const [songList, setSongList] = useState([]);
   const [songName, setSongName] = useState("Laal Dupatta - Mujhse Shaadi Karogi");
-  let isPlaying = false;
+  const [playing, setPlaying] = useState(false);
   const music = document.getElementById('audio');
   const list = [...songList];
+  const [progressValue, setProgressValue] = useState(0);
+  const [loadSong, setLoadSong] = useState(true);
+  const isMount = useRef(false);
 
   const getData = (e) => {
     getDownloadURL(ref(storage, `${e}.mp3`))
     .then((url) => {
       setSongUrl(url);
       // console.log(url);
+      setLoadSong(false);
     })
   };
 
@@ -33,6 +37,17 @@ function App() {
   }, [songName]);
 
   useEffect(() => {
+    if (isMount.current) {
+      if (!loadSong) {
+        playSong();
+      }
+    }
+    else{
+      isMount.current = true;
+    }
+  }, [loadSong]);
+
+  useEffect(() => {
     getSongs();
   }, []);
 
@@ -41,23 +56,20 @@ function App() {
     pauseSong();
     music.currentTime = 0;
     // console.log(e);
+    setLoadSong(true);
   }
 
   const playSong = () => {
-    isPlaying = true;
+    setPlaying(true);
     music.play();
-    document.querySelector('#play-pause').classList.remove('fa-play');
-    document.querySelector('#play-pause').classList.add('fa-pause');
   }
   const pauseSong = () => {
-    isPlaying = false;
+    setPlaying(false);
     music.pause();
-    document.querySelector('#play-pause').classList.remove('fa-pause');
-    document.querySelector('#play-pause').classList.add('fa-play');
   }
 
   const handleSong = () => {
-    isPlaying ? pauseSong() : playSong();
+    playing ? pauseSong() : playSong();
   }
 
   const nextSong = (e) => {
@@ -69,6 +81,7 @@ function App() {
         pauseSong();
         music.currentTime = 0;
         // console.log(songName);
+        setLoadSong(true);
       }
     });
   }
@@ -81,9 +94,35 @@ function App() {
         pauseSong();
         music.currentTime = 0;
         // console.log(songName);
+        setLoadSong(true);
       }
     });
   }
+
+  const timeUpdate = (e) => {
+    // console.log(e);
+    const {currentTime, duration} = e.target;
+    // console.log(currentTime, duration);
+    const progress = Math.floor((currentTime / duration) * 100);
+    setProgressValue(progress);
+  }
+
+  const skipSong = (e) => {
+    // console.log(e);
+    const {duration} = music;
+    let skipMusic = (e.nativeEvent.offsetX / e.target.clientWidth) * duration;
+    // console.log(skipMusic);
+    music.currentTime = skipMusic;
+  }
+
+  document.addEventListener('keydown', (e) => {
+    // console.log(e);
+    if (e.keyCode === 32) {
+      handleSong();
+    }
+  })
+
+  
 
   // console.log(storage);
 
@@ -95,21 +134,27 @@ function App() {
         </figure>
           <h1 className='font-semibold text-green-500 text-2xl'>Spotify</h1>
       </header>
-      <audio id='audio' src={songUrl}></audio>
+      <audio id='audio' src={songUrl} onTimeUpdate={timeUpdate} onEnded={() => nextSong(list)}></audio>
       <ul className='flex-grow bg-gray-900 overflow-y-auto'>
         {/* <li onClick={() => handleChange('file_example')}>file_example</li> */}
         {songList.map((song_name, id) => {
           return(
-            <li className='py-4 px-5 border-b border-green-800 hover:bg-gray-800 text-gray-400 cursor-pointer' key={id} onClick={() => handleChange((song_name._location.path_).replace('.mp3', ''))}>{song_name._location.path_}</li>
+            <li className='py-4 px-5 border-b border-green-800 lg:hover:bg-gray-800 text-gray-400 cursor-pointer' key={id} onClick={() => handleChange((song_name._location.path_).replace('.mp3', ''))}>{song_name._location.path_}</li>
           )
         })}
       </ul>
-      <div className='player border-t border-white p-5 bg-gray-900 text-white flex flex-col sm:flex-row gap-10 sm:gap-16 items-center'>
+      <div className='player border-t border-white p-5 bg-gray-900 text-white flex flex-col sm:flex-row gap-6 sm:gap-16 items-center'>
         <h1>{songName}</h1>
-        <div className='controls flex gap-10'>
-          <span className='fa fa-step-backward cursor-pointer' onClick={() => prevSong(list)} title='previous'></span>
-          <span id='play-pause' className='fa fa-play cursor-pointer' onClick={() => handleSong()} title="play/pause"></span>
-          <span className='fa fa-step-forward cursor-pointer' onClick={() => nextSong(list)} title='next'></span>
+        <div className='controls flex items-center flex-col-reverse sm:flex-row gap-8'>
+          <div className='flex items-center gap-10'>
+            <span className='fa fa-step-backward cursor-pointer' onClick={() => prevSong(list)} title='previous'></span>
+            {
+              loadSong ? (<span className='loader'></span>) : 
+              (<span id='play-pause' className={playing ? "fa fa-pause cursor-pointer" : "fa fa-play cursor-pointer"} onClick={() => handleSong()} title="play/pause"></span>)
+            }
+            <span className='fa fa-step-forward cursor-pointer' onClick={() => nextSong(list)} title='next'></span>
+          </div>
+          <progress className='cursor-pointer h-2' value={progressValue} max="100" onClick={skipSong} />
         </div>
       </div>
     </div>
